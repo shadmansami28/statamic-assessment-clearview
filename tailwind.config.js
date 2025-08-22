5cbf3b84739f778c91b6e88542a0bacd779ef071
shadmansami28 <158273444+shadmansami28@users.noreply.github.com>
2025-08-22 20:17:37 +0600

    Your descriptive commit message

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./resources/**/*.antlers.html",
+    "./resources/**/*.antlers.php", 
+    "./resources/**/*.blade.php",
+    "./resources/**/*.vue",
+    "./resources/**/*.js",
+    "./content/**/*.md",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
